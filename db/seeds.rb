74a56dc70a7c7037818d7b444b0b314a98208eca
Ramona Maltan <ramonamaltan@gmail.com>
2020-11-12 13:06:58 +0100

    pre lunch finished index working on show action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'destroy all'
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts 'start seeding ingredients'
+
+Ingredient.create(name: "lemon juice")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "basil leaves")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "cranberry juice")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "peach schnapps")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "cointreau")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "sugar sirup")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "tabasco")
+Ingredient.create(name: "worcester sauce")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "coke")
+
+puts "seeded #{Ingredient.count} ingredients"
+
+
+puts "start seeding cocktails"
+
+Cocktail.create(name: "Basil Smash")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Long Island IceTea")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Margherita")
+
+puts "seeded #{Cocktail.count} cocktails"
